34f6aeffeef81f962c5df80312e5bc0ba4b6d661
JunYoung Lee <52552781+junlight94@users.noreply.github.com>
2021-08-29 13:12:03 +0900

    news app first commit

---
+++ NewsApp/Type1.swift
@@ -0,0 +1,14 @@
+//
+//  Type1.swift
+//  NewsApp
+//
+//  Created by Junyoung Lee on 2021/08/29.
+//
+
+import UIKit
+
+class Type1 : UITableViewCell {
+    //label
+    
+    @IBOutlet weak var LabelText: UILabel!
+}
--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -7,11 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var TableViewMain: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // as? as! 부모 자식 친자확인
+        let cell = TableViewMain.dequeueReusableCell(withIdentifier: "Type1", for: indexPath) as! Type1
+        
+        cell.LabelText.text = "\(indexPath.row)"
+        
+        return cell
+    }
+    
+    //클릭
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath.row)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        TableViewMain.delegate = self
+        TableViewMain.dataSource = self
     }
 
 
